191dd025767db5ca00be921188d7981578818050
CorgiTaco <66983020+CorgiTaco@users.noreply.github.com>
2021-02-25 20:48:14 -0800

    -Init

---
+++ src/main/java/corgitaco/mergedmappings/ExampleMod.java
@@ -0,0 +1,11 @@
+package corgitaco.mergedmappings;
+
+import net.fabricmc.api.ModInitializer;
+
+public class ExampleMod implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
